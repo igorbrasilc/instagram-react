a7ba5209cf208807046e7a934269b498f6116be2
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-03-10 19:14:35 -0300

    Navbar, Corpo e Fundo lançados via React

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom";
 import Navbar from "./Navbar";
+import Corpo from "./Corpo";
+import FundoMobile from "./FundoMobile";
 
 function App() {
     return (
         <div>
             <Navbar />
-            {/* <Corpo /> */}
+            <Corpo />
+            <FundoMobile />
         </div>
     )
 }

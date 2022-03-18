67fb2d990cfdaf85b3f4bb2504e8906993e1f8b2
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-03-18 10:31:59 -0300

    Preparando revisão de código

--- src/components/FundoMobile.js
+++
@@ -1,8 +0,0 @@
-export default function FundoMobile() {
-    const icones = ["home", "search-outline", "add-circle-outline", "heart-outline", "person-outline"];
-    return (
-    <div class="fundo-mobile">
-        {icones.map(icone => <ion-icon name={icone}></ion-icon>)}
-    </div>
-    )
-}
